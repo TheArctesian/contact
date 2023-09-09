b5b7663a406d80661ad3f94c8fef98082f6d8ce5
theArctesian <stephen.d.okita@gmail.com>
2023-09-09 13:47:03 -0700

    fuck this

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import "../app.css";
+  import { onMount } from "svelte";
+  import vCard from "vcf";
+  let contactData;
+
+
+  onMount(() => {
+    const link = document.createElement('a');
+    link.href = '/StephenOkita.vcf';
+    link.download = 'StephenOkita.vcf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
+</script>
+
+<!-- <button on:click={saveFile}>Save File</button> -->
+<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+  <a href="/StephenOkita.vcf" download> Save Contact</a>
+</div>
+  <!-- Display other relevant contact information -->
+
+<style>
+ a{
+    background-color: var(--purple);
+    padding: 2rem;
+ }
+</style>
