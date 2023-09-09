15c6bd0b9da44177169b286e5d05ad6f348c9175
The Arctesian <87273292+TheArctesian@users.noreply.github.com>
2023-09-09 12:12:26 -0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import "../app.css";
+</script>
+
+<slot />
+
+<style>
+
+:global(body) {
+    background-color: var(--bg);
+    font-family: "Fira Code", monospace;
+    letter-spacing: 0.1em; /* Adjust as needed */
+    color: var(--white);
+  }
+
+</style>
