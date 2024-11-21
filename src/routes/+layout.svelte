2d60a396b47d8ded21fa710beb781a75564a1d04
theArctesian <stephen.d.okita@gmail.com>
2024-11-21 01:49:10 -0800

    anal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,18 @@
 <script>
   import "../app.css";
+  import { dev } from "$app/environment";
+  import { injectAnalytics } from "@vercel/analytics/sveltekit";
+
+  injectAnalytics({ mode: dev ? "development" : "production" });
 </script>
 
 <slot />
 
 <style>
-
-:global(body) {
+  :global(body) {
     background-color: var(--bg);
     font-family: "Fira Code", monospace;
     letter-spacing: 0.1em; /* Adjust as needed */
     color: var(--white);
   }
-
 </style>
